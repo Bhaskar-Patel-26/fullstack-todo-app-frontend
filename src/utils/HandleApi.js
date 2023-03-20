009a9a2f4ee60b08a5a6fa8a13feaadd9133655b
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2023-03-20 19:12:10 +0530

    Update HandleApi.js
    baseUrl updated

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://fullstack-todo-app-backend-6011.onrender.com";
+const baseUrl = "https://fullstack-todo-app-backend-misu.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
